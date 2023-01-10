0a5228f98bce282ead31a1af33631acdd1e67d3a
osatou0141 <gbtikin@gmail.com>
2023-01-10 21:21:43 +0900

    itkidsM5消しました

--- itrobot/kadai_osatou0141/kadai_06.py
+++ itrobot/kadai_osatou0141/kadai_06.py
@@ -1,6 +1,6 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
-
+import random as rd
 from time import sleep
 
 mc = Minecraft.create(port=param.PORT_MC)
@@ -10,12 +10,9 @@ x = 10
 y = 63
 z = 5
 
-A = param.GLASS
-B = param.SEA_LANTERN_BLOCK
-C = param.GLOWSTONE
-
 for n in range(8):
     for i in range(10):
+        A = rd.randint(1, 3)
         mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
         sleep(0.1)
         z += 1
