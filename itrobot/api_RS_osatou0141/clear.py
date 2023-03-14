a68d80ccc82e718e2eb5dc26f8d304b2d36da00d
osatou0141 <gbtikin@gmail.com>
2023-03-14 21:17:40 +0900

    土台のリセットをsheep.pyに組み込んだ試作段階

--- itrobot/api_RS_osatou0141/clear.py
+++ itrobot/api_RS_osatou0141/clear.py
@@ -1,11 +1,10 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
 
-mc = Minecraft.create(port=param.PORT_MC)
-mc.postToChat('Will all clear after this !')
-mc.postToChat('And, make a GLASS field !')
 
-mc.setBlocks(-80, 63, -80,   80, 128, 80,   param.AIR)
-mc.setBlocks(-80, 62, -80,   80, 62, 80,   param.GRASS_BLOCK)
-
-mc.postToChat('Finish Now !!')
+def reset():
+    mc = Minecraft.create(port=param.PORT_MC)
+    mc.postToChat('clear and make pallete')
+    mc.setBlocks(-50, 63, -50,   50, 150, 50,   param.AIR)
+    mc.setBlocks(-50, 63, -50,   50, 63, 50,   param.GRASS_BLOCK)
+    mc.postToChat('finish')
--- itrobot/api_RS_osatou0141/sheep.py
+++ itrobot/api_RS_osatou0141/sheep.py
@@ -1,30 +1,31 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
+from clear import reset
 
 mc = Minecraft.create(port=param.PORT_MC)
 
 
-def palette(x=0, y=63, z=0):
+def palette(x=0, y=64, z=0):
     mc.setBlocks(x - 14, y - 1, z - 2, x + 33, y - 1, z + 21, param.SEA_LANTERN_BLOCK)
 
 
-def leg(x=0, y=63, z=0):
+def leg(x=0, y=64, z=0):
     mc.setBlocks(x, y, z, x + 7, y + 1, z + 7, param.BROWN_TERRACOTTA)
     mc.setBlocks(x + 2, y, z + 2, x + 7, y + 1, z + 5, param.WHITE_TERRACOTTA)
     mc.setBlocks(x, y + 2, z, x + 7, y + 11, z + 7, param.WHITE_TERRACOTTA)
     mc.setBlocks(x - 1, y + 12, z - 1, x + 8, y + 21, z + 8, param.WHITE_WOOL)
 
 
-def body(x=0, y=63, z=0):
+def body(x=0, y=64, z=0):
     mc.setBlocks(x - 6, y + 22, z - 2, x + 33, y + 39, z + 21, param.WHITE_WOOL)
 
 
-def head(x=0, y=63, z=0):
+def head(x=0, y=64, z=0):
     mc.setBlocks(x + 1, y + 32, z + 3, x - 12, y + 45, z + 16, param.WHITE_WOOL)
     mc.setBlocks(x - 14, y + 33, z + 4, x - 13, y + 44, z + 15, param.WHITE_WOOL)
 
 
-def face(x=0, y=63, z=0):
+def face(x=0, y=64, z=0):
     mc.setBlocks(x - 14, y + 37, z + 4, x - 13, y + 42, z + 15, param.WHITE_TERRACOTTA)
     mc.setBlocks(x - 14, y + 33, z + 6, x - 13, y + 36, z + 13, param.WHITE_TERRACOTTA)
     mc.setBlocks(x - 14, y + 33, z + 8, x - 14, y + 36, z + 11, param.PINK_CONCRETE)
@@ -35,14 +36,15 @@ def face(x=0, y=63, z=0):
 
 
 def makesheep():
-    palette()
-    leg()
-    leg(z=12)
-    leg(x=24)
-    leg(x=24, z=12)
+    palette()        # 土台
+    leg()            # 右前足
+    leg(z=12)        # 左前足
+    leg(x=24)        # 右後足
+    leg(x=24, z=12)  # 左後足
     body()
     head()
     face()
 
 
+reset()     # clear.pyのresetモジュール
 makesheep()
