6dcb8b014b8032bf35d3cb16707d59cae5355fd3
osatou0141 <gbtikin@gmail.com>
2023-03-28 20:40:45 +0900

    ts

--- itrobot/api_RS_osatou0141/clear.py
+++ itrobot/api_RS_osatou0141/clear.py
@@ -2,9 +2,9 @@ from mcje.minecraft import Minecraft
 import param_MCJE as param
 
 
-class reset:
+def reset(x=0, y=63, z=0):
     mc = Minecraft.create(port=param.PORT_MC)
     mc.postToChat('clear and make pallete')
-    mc.setBlocks(-50, 62, -50,   50, 150, 50,   param.AIR)
+    mc.setBlocks(-50, 63, -50,   50, 150, 50,   param.AIR)
     mc.setBlocks(-50, 62, -50,   50, 62, 50,   param.GRASS_BLOCK)
     mc.postToChat('finish')
