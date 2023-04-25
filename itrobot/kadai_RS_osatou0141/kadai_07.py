aa471052e519b12a7a0f884f7832bc7fb6a690eb
osatou0141 <gbtikin@gmail.com>
2023-04-25 21:32:11 +0900

    課題７の階段関係

--- itrobot/kadai_RS_osatou0141/kadai_07.py
+++ itrobot/kadai_RS_osatou0141/kadai_07.py
@@ -11,6 +11,14 @@ head = 0
 x = -26
 y = 63
 z = -3
+muki = 0
+kx = -10
+ky = 63
+kz = -20
+ue = 0
+migi = 0
+sita = 0
+hidari = 0
 
 
 def skull(x, y, z):
@@ -36,6 +44,14 @@ def creeper(x, y, z):
     mc.setBlocks(x, y + 4, z + 5, x, y + 5, z + 6, param.BLACK_CONCRETE)
 
 
+def kaidan(kx, y, kz):
+    mc.setBlocks(kx, y, kz, kx - 4, y, kz - 4, param.SMOOTH_STONE)
+
+
+def clear(kx, ky, kz):
+    mc.setBlocks(kx - 15, ky, kz - 15, kx + 15, ky + 5, kz + 15, param.AIR)
+
+
 for n in range(3):
     head = rd.randint(1, 3)
     if head == 1:
@@ -46,3 +62,37 @@ for n in range(3):
         creeper(x, y, z)
     sleep(0.1)
     z += 8
+
+mc.postToChat("壁作った！！！！！！")
+clear(kx, ky, kz)
+
+for m in range(5):
+    kaidan(kx, y, kz)
+    y += 1
+    muki = rd.randint(1, 4)
+    if muki == 1 & ue == 0:
+        kz += -5
+        ue = 0
+        migi = 0
+        sita = 1
+        hidari = 0
+    elif muki == 2 & migi == 0:
+        kx += 5
+        ue = 0
+        migi = 0
+        sita = 0
+        hidari = 1
+    elif muki == 3:
+        kz += 5
+        ue = 1
+        migi = 0
+        sita = 0
+        hidari = 0
+    else:
+        kx += -5
+        ue = 0
+        migi = 1
+        sita = 0
+        hidari = 0
+    sleep(0.1)
+    mc.postToChat("階段置いた！！！！！")
