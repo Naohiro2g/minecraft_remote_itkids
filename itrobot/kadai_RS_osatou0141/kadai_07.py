201a7d1b6f86f2d9aa0efb39220e706287dca9c3
osatou0141 <gbtikin@gmail.com>
2023-07-11 21:29:45 +0900

    kadai7

--- itrobot/kadai_RS_osatou0141/kadai_07.py
+++ itrobot/kadai_RS_osatou0141/kadai_07.py
@@ -11,14 +11,10 @@ head = 0
 x = -26
 y = 63
 z = -3
-muki = 0
-kx = -10
+kx = -15
 ky = 63
-kz = -20
-ue = 0
-migi = 0
-sita = 0
-hidari = 0
+kz = -10
+muki = 0
 
 
 def skull(x, y, z):
@@ -44,12 +40,11 @@ def creeper(x, y, z):
     mc.setBlocks(x, y + 4, z + 5, x, y + 5, z + 6, param.BLACK_CONCRETE)
 
 
-def kaidan(kx, y, kz):
-    mc.setBlocks(kx, y, kz, kx - 4, y, kz - 4, param.SMOOTH_STONE)
-
-
-def clear(kx, ky, kz):
-    mc.setBlocks(kx - 15, ky, kz - 15, kx + 15, ky + 5, kz + 15, param.AIR)
+def maekaidan(kx, ky, kz):
+    mc.setBlocks(kx, ky, kz, kx + 5, ky, kz - 1, param.SMOOTH_STONE)
+    mc.setBlocks(kx, ky + 1, kz - 2, kx + 5, ky + 1, kz - 3, param.SMOOTH_STONE)
+    mc.setBlocks(kx, ky + 2, kz - 4, kx + 5, ky + 2, kz - 5, param.SMOOTH_STONE)
+    mc.setBlocks(kx, ky + 3, kz - 6, kx + 5, ky + 3, kz - 11, param.SMOOTH_STONE)
 
 
 for n in range(3):
@@ -64,35 +59,16 @@ for n in range(3):
     z += 8
 
 mc.postToChat("壁作った！！！！！！")
-clear(kx, ky, kz)
 
-for m in range(5):
-    kaidan(kx, y, kz)
-    y += 1
-    muki = rd.randint(1, 4)
-    if muki == 1 & ue == 0:
-        kz += -5
-        ue = 0
-        migi = 0
-        sita = 1
-        hidari = 0
-    elif muki == 2 & migi == 0:
-        kx += 5
-        ue = 0
-        migi = 0
-        sita = 0
-        hidari = 1
-    elif muki == 3:
-        kz += 5
-        ue = 1
-        migi = 0
-        sita = 0
-        hidari = 0
-    else:
-        kx += -5
-        ue = 0
-        migi = 1
-        sita = 0
-        hidari = 0
-    sleep(0.1)
-    mc.postToChat("階段置いた！！！！！")
+maekaidan(kx, ky, kz)
+ky += 4
+kz -= 6
+muki = rd.randint(1, 3)
+if muki == 1:
+    hidarikaidan()
+elif muki == 2:
+    maekaidan(kx, ky, kz)
+else:
+    migikaidan()
+
+mc.postToChat("階段置いた！！！！！")
