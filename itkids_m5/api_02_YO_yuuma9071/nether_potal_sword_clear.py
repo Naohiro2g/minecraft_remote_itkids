e5bd7120c06cef0fefb7b8c12675c2becb7c7129
yuuma9071 <98243482+yuuma9071@users.noreply.github.com>
2023-08-19 23:40:52 +0900

    Update nether_potal_sword_clear.py

--- itkids_m5/api_02_YO_yuuma9071/nether_potal_sword_clear.py
+++ itkids_m5/api_02_YO_yuuma9071/nether_potal_sword_clear.py
@@ -10,17 +10,9 @@ sleep(1)
 mc.postToChat('ネザーゲートを破壊します。')
 sleep(2)
 
+x, z = 1, 6
 y = param.Y_SEA + 1
-for _n in range(31):
-    x = 1
-    for _n in range(9):
-        z = 6
-        for _i in range(13):
-            mc.setBlock(x, y, z,  param.GLOWSTONE)
-            sleep(0.001)
-            z += 1
-        x += 1
-    y += 1
+mc.setBlocks(x, y, z, x + 8, y + 30, z + 12, param.GLOWSTONE)
 
 sleep(1)
 
