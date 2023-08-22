ff0f51913cda57c86374a0c4fc463b2d238d590e
osatou0141 <gbtikin@gmail.com>
2023-08-22 21:25:32 +0900

    ts

--- itrobot/kadai_RS_osatou0141/kadai_07.py
+++ itrobot/kadai_RS_osatou0141/kadai_07.py
@@ -7,6 +7,8 @@ from time import sleep
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat("kadai7")
 
+
+o = 1
 head = 0
 x = -26
 y = 63
@@ -40,17 +42,6 @@ def creeper(x, y, z):
     mc.setBlocks(x, y + 4, z + 5, x, y + 5, z + 6, param.BLACK_CONCRETE)
 
 
-def odoriba(kx, ky, kz):
-    mc.setBlocks(kx, ky, kz - 6, kx + 5, ky, kz - 11, param.SMOOTH_STONE)
-
-
-def maekaidan(kx, ky, kz):
-    mc.setBlocks(kx, ky, kz, kx + 5, ky, kz - 1, param.SMOOTH_STONE)
-    mc.setBlocks(kx, ky + 1, kz - 2, kx + 5, ky + 1, kz - 3, param.SMOOTH_STONE)
-    mc.setBlocks(kx, ky + 2, kz - 4, kx + 5, ky + 2, kz - 5, param.SMOOTH_STONE)
-    mc.setBlocks(kx, ky + 3, kz - 6, kx + 5, ky + 3, kz - 11, param.SMOOTH_STONE)
-
-
 for n in range(3):
     head = rd.randint(1, 3)
     if head == 1:
@@ -63,16 +54,3 @@ for n in range(3):
     z += 8
 
 mc.postToChat("壁作った！！！！！！")
-
-maekaidan(kx, ky, kz)
-ky += 4
-kz -= 6
-muki = rd.randint(1, 3)
-if muki == 1:
-    hidarikaidan()
-elif muki == 2:
-    maekaidan(kx, ky, kz)
-else:
-    migikaidan()
-
-mc.postToChat("階段置いた！！！！！")
