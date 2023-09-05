f16b923f8c01cd786525f048a08d369d48638627
osatou0141 <gbtikin@gmail.com>
2023-09-05 20:48:47 +0900

    名称変更

---
+++ itrobot/api_RS_osatou0141/reset.py
@@ -0,0 +1,15 @@
+from mcje.minecraft import Minecraft
+import param_MCJE as param
+
+mc = Minecraft.create(port=param.PORT_MC)
+
+
+def reset(x=0, y=63, z=0):
+    mc = Minecraft.create(port=param.PORT_MC)
+    mc.postToChat('clear and make pallete')
+    mc.setBlocks(x - 50, y, z - 50,   x + 50, y + 87, z + 50,   param.AIR)
+    mc.setBlocks(x - 50, y - 1, z - 50,   x + 50, y - 1, z + 50,   param.GRASS_BLOCK)
+    mc.postToChat('finish')
+
+
+reset()
