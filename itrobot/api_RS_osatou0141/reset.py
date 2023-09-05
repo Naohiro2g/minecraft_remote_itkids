a5a9e722d1e14af1abd28c1f5fb4c881c0e90a1b
osatou0141 <gbtikin@gmail.com>
2023-09-05 21:24:18 +0900

    初期化プログラムの調整

--- itrobot/api_RS_osatou0141/reset.py
+++ itrobot/api_RS_osatou0141/reset.py
@@ -6,9 +6,9 @@ mc = Minecraft.create(port=param.PORT_MC)
 
 def reset(x=0, y=63, z=0):
     mc = Minecraft.create(port=param.PORT_MC)
-    mc.postToChat('clear and make pallete')
-    mc.setBlocks(x - 50, y, z - 50,   x + 50, y + 87, z + 50,   param.AIR)
-    mc.setBlocks(x - 50, y - 1, z - 50,   x + 50, y - 1, z + 50,   param.GRASS_BLOCK)
+    mc.postToChat('reset field')
+    mc.setBlocks(x - 35, y, z - 35, x + 65, y + 100, z + 65, param.AIR)
+    mc.setBlocks(x - 35, y - 1, z - 35, x + 65, y - 1, z + 65, param.GRASS_BLOCK)
     mc.postToChat('finish')
 
 
